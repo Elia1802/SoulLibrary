2c0d2aa5bce66e628dc66680b224e15588d499f5
Elia <98826251+Elia1802@users.noreply.github.com>
2025-01-20 14:14:02 +0100

    Update Versions

--- src/main/java/de/elia/api/PluginInfo.java
+++ src/main/java/de/elia/api/PluginInfo.java
@@ -3,9 +3,9 @@ package de.elia.api;
 public class PluginInfo {
 
   public static final String NAME = "SoulLibrary";
-  public static final String VERSION = "3.0.1";
-  public static final String API_NAME = "Paper-API";
-  public static final String API_VERSION = "1.20.4-R0.1-SNAPSHOT";
+  public static final String VERSION = "5.0.0-dev";
+  public static final String API_NAME = "Paper-Dev-Bundle";
+  public static final String API_VERSION = "1.21.4-R0.1-20241205.222346-7";
   public static final String AUTHOR = "Elia, Zopnote";
 
 }
