68e1eed9e8a14d2fd6a2f4964f40ac941b10d752
Elia <98826251+Elia1802@users.noreply.github.com>
2025-01-26 17:04:15 +0100

    Update to the paper fork

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 
 plugins {
   `java-library`
-  id("io.papermc.paperweight.userdev") version "1.7.3"
+  id("io.papermc.paperweight.userdev") version "2.0.0-beta.14"
   //id("xyz.jpenilla.run-paper") version "2.3.1" // Adds runServer and runMojangMappedServer tasks for testing
   //id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.2.0" // Generates plugin.yml based on the Gradle config
 }
@@ -30,7 +30,7 @@ tasks.assemble {
  */
 
 dependencies {
-  paperweight.paperDevBundle("1.21.4-R0.1-20241205.222346-7")
+  paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
   // paperweight.foliaDevBundle("1.21.1-R0.1-SNAPSHOT")
   // paperweight.devBundle("com.example.paperfork", "1.21.1-R0.1-SNAPSHOT")
 }
